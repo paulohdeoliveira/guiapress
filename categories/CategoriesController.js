20bc06408b9058c06d6e173dacc7d52e02db61cf
Paulo Henique de Oliveira <pauloh.deoliveira@outlook.com>
2023-03-08 12:02:09 -0300

    Implementado editar categoria

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -46,4 +46,39 @@ router.get("/admin/categories", (req, res) => {
     });
 });
 
+router.get("/admin/categories/edit/:id", (req, res) => {
+    let id = req.params.id;
+    if (isNaN(id)) {
+        res.redirect("/admin/categories");
+    }
+    Category.findByPk(id)
+        .then((category) => {
+            if (category != undefined) {
+                res.render("admin/categories/edit", {
+                    category: category,
+                });
+            } else {
+                res.redirect("/admin/categories");
+            }
+        })
+        .catch((error) => {
+            res.redirect("/admin/categories");
+        });
+});
+
+router.post("/categories/update", (req, res) => {
+    let id = req.body.id;
+    let title = req.body.title;
+    Category.update(
+        { title: title, slug: slugify(title) },
+        {
+            where: {
+                id: id,
+            },
+        }
+    ).then(() => {
+        res.redirect("/admin/categories");
+    });
+});
+
 module.exports = router;
