66d5a011c405388291df33cf5470a14affd08d06
Paulo Henique de Oliveira <pauloh.deoliveira@outlook.com>
2023-03-09 11:02:57 -0300

    Conclusão da  página de artigos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -4,11 +4,15 @@ const Category = require("../categories/Category");
 const Article = require("./Article");
 const slugify = require("slugify");
 
-router.get("/articles", (req, res) => {
-    res.send("Rota de artigos");
+router.get("/admin/articles", (req, res) => {
+    Article.findAll({
+        include: [{ model: Category }],
+    }).then((articles) => {
+        res.render("admin/articles/index", { articles: articles });
+    });
 });
 
-router.get("/admin/articles", (req, res) => {
+router.get("/admin/articles/new", (req, res) => {
     Category.findAll().then((categories) => {
         res.render("admin/articles/new", { categories: categories });
     });
@@ -27,6 +31,25 @@ router.post("/articles/save", (req, res) => {
     }).then(() => {
         res.redirect("/admin/articles");
     });
+});
+
+router.post("/articles/delete", (req, res) => {
+    let id = req.body.id;
+    if (id != undefined) {
+        if (!isNaN(id)) {
+            Article.destroy({
+                where: {
+                    id: id,
+                },
+            }).then(() => {
+                res.redirect("/admin/articles");
+            });
+        } else {
+            res.redirect("/admin/articles");
+        }
+    } else {
+        res.redirect("/admin/articles");
+    }
 });
 
 module.exports = router;
