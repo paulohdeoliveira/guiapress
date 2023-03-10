57d014d9841d3be7dd9274ce9b5c5db2b85e98e7
Paulo Henique de Oliveira <pauloh.deoliveira@outlook.com>
2023-03-10 13:21:19 -0300

    Finalizado editar artigo

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -14,7 +14,9 @@ router.get("/admin/articles", (req, res) => {
 
 router.get("/admin/articles/new", (req, res) => {
     Category.findAll().then((categories) => {
-        res.render("admin/articles/new", { categories: categories });
+        res.render("admin/articles/new", {
+            categories: categories,
+        });
     });
 });
 
@@ -50,6 +52,51 @@ router.post("/articles/delete", (req, res) => {
     } else {
         res.redirect("/admin/articles");
     }
+});
+
+router.get("/admin/articles/edit/:id", (req, res) => {
+    let id = req.params.id;
+    Article.findByPk(id)
+        .then((article) => {
+            if (article != undefined) {
+                Category.findAll().then((categories) => {
+                    res.render("admin/articles/edit", {
+                        article,
+                        categories,
+                    });
+                });
+            } else {
+                res.redirect("/");
+            }
+        })
+        .catch((err) => {
+            res.redirect("/");
+        });
+});
+
+router.post("/articles/update", (req, res) => {
+    let id = req.body.id;
+    let title = req.body.title;
+    let body = req.body.body;
+    let category = req.body.category;
+
+    Article.update(
+        {
+            title: title,
+            body: body,
+            categoryId: category,
+            slug: slugify(title),
+        },
+        {
+            where: { id: id },
+        }
+    )
+        .then(() => {
+            res.redirect("/admin/articles");
+        })
+        .catch((error) => {
+            res.redirect("/admin/articles");
+        });
 });
 
 module.exports = router;
